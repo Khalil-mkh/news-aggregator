7e6e7a3bf75a7e67e59f83a6a0061a8b59bc3957
khalil.m.mkh <khalil.m.mkh@gmail.com>
2025-02-23 20:30:37 +0300

    feat: add home and for-you pages

---
+++ src/features/forYouNews/pages/index.tsx
@@ -0,0 +1,210 @@
+import { isNull } from "lodash";
+import {
+  useGetAllGuardianArticles,
+  useGetNewsAPIArticles,
+  useGetNewsOrgArticles,
+} from "../../../shared/services";
+import { useSearchStore } from "../../../shared/store/searchStore";
+import { normalizeArticles } from "../../../shared/helpers/normalizeArticles";
+import { useMemo, useState } from "react";
+import { usePreferencesStore } from "../../userPreferences/store/preferencesStore";
+import { ArticleModel } from "../../../shared/types/articleModel";
+import NewsCard from "../../../shared/components/newsCard";
+import AppLayout from "../../../shared/components/appLayout";
+import Modal from "../../../shared/components/modal";
+import { usePreferencesModalStore } from "../../../shared/store/preferencesModalStore";
+import { Categories } from "../../userPreferences/config/categories";
+import { Sources } from "../../userPreferences/config/sources";
+import Loader from "../../../shared/components/loader";
+import NoData from "../../../shared/components/noData";
+
+const ForYouNewsPage = () => {
+  const { preferences, setPreferences } = usePreferencesStore();
+  const { searchValue } = useSearchStore();
+  const { open, setOpen } = usePreferencesModalStore();
+
+  const [activeTab, setActiveTab] = useState("tab1");
+
+  const [selectedCategories, setSelectedCategories] = useState<string[]>(
+    preferences?.categories || []
+  );
+  const [selectedSources, setSelectedSources] = useState<string[]>(
+    preferences?.sources || []
+  );
+
+  const handleCategoriesChange = (option: string) => {
+    if (selectedCategories === null) {
+      setSelectedCategories([option]);
+    } else {
+      const newValues = selectedCategories.includes(option)
+        ? selectedCategories.filter((c) => c !== option)
+        : [...selectedCategories, option];
+
+      setSelectedCategories(newValues);
+    }
+  };
+
+  const handleSourcesChange = (option: string) => {
+    if (selectedSources === null) {
+      setSelectedSources([option]);
+    } else {
+      const newValues = selectedSources.includes(option)
+        ? selectedSources.filter((c) => c !== option)
+        : [...selectedSources, option];
+
+      setSelectedSources(newValues);
+    }
+  };
+
+  const handleSubmitPreferences = () => {
+    setPreferences({
+      categories: selectedCategories,
+      sources: selectedSources,
+    });
+    setOpen(false);
+  };
+
+  const { data: newsOrgArticles, isLoading: isNewsOrgArticlesLoading } =
+    useGetNewsOrgArticles(
+      {
+        params: {
+          category: preferences?.categories?.[0],
+          q: searchValue,
+        },
+      },
+      {
+        enabled:
+          isNull(preferences?.sources) ||
+          preferences.sources?.includes("news-api-org"),
+      }
+    );
+
+  const { data: newsApiArticles, isLoading: isNewsAPIArticlesLoading } =
+    useGetNewsAPIArticles(
+      {
+        params: {
+          q: searchValue,
+          categories: preferences?.categories?.join(","),
+        },
+      },
+      {
+        queryKey: [{ url: "1" }],
+        enabled:
+          isNull(preferences?.sources) ||
+          preferences.sources?.includes("news-api"),
+      }
+    );
+
+  const { data: guardianArticles, isLoading: isGuardianArticlesLoading } =
+    useGetAllGuardianArticles(
+      {
+        params: {
+          q: searchValue,
+          categories: preferences?.categories?.join(","),
+        },
+      },
+      {
+        enabled:
+          isNull(preferences?.sources) ||
+          preferences.sources?.includes("guardian-api"),
+      }
+    );
+
+  const articles = useMemo(() => {
+    const allArticles = normalizeArticles(
+      newsOrgArticles,
+      newsApiArticles,
+      guardianArticles
+    );
+
+    if (preferences?.sources?.includes("news-api-org")) {
+      return allArticles.filter((article) => article.dataSource === "news-org");
+    } else if (preferences?.sources?.includes("guardian-api")) {
+      return allArticles.filter((article) => article.dataSource === "guardian");
+    } else if (preferences?.sources?.includes("news-api")) {
+      return allArticles.filter((article) => article.dataSource === "news-api");
+    } else {
+      return allArticles;
+    }
+  }, [
+    newsOrgArticles,
+    newsApiArticles,
+    guardianArticles,
+    preferences?.sources,
+  ]);
+
+  const isLoading =
+    isNewsOrgArticlesLoading ||
+    isNewsAPIArticlesLoading ||
+    isGuardianArticlesLoading;
+
+  return (
+    <AppLayout showUpdatePreferences>
+      {isLoading && <Loader />}
+      {!isLoading && articles.length === 0 && <NoData />}
+      <div className="grid md:grid-cols-3 gap-16 mt-24">
+        {articles?.map((article: ArticleModel) => {
+          return <NewsCard article={article} />;
+        })}
+      </div>
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        title="Your Preferences"
+      >
+        <div className="flex border-b mb-4">
+          {["tab1", "tab2", "tab3"].map((tab) => (
+            <button
+              key={tab}
+              onClick={() => setActiveTab(tab)}
+              className={`py-2 px-4 w-1/3 text-center ${
+                activeTab === tab
+                  ? "border-b-2 border-blue-500 text-blue-600"
+                  : "text-gray-500"
+              }`}
+            >
+              {tab.toUpperCase()}
+            </button>
+          ))}
+        </div>
+        {/* Tab Content */}
+        <div className="text-gray-700">
+          {activeTab === "tab1" &&
+            Categories.map((option) => (
+              <label key={option.key} className="flex items-center space-x-3">
+                <input
+                  type="checkbox"
+                  checked={selectedCategories?.includes(option.key) || false}
+                  onChange={() => handleCategoriesChange(option.key)}
+                />
+                <span>{option.name}</span>
+              </label>
+            ))}
+          {activeTab === "tab2" &&
+            Sources.map((option) => (
+              <label key={option.key} className="flex items-center space-x-3">
+                <input
+                  type="checkbox"
+                  checked={selectedSources?.includes(option.key) || false}
+                  onChange={() => handleSourcesChange(option.key)}
+                />
+                <span>{option.name}</span>
+              </label>
+            ))}
+          {/* {activeTab === "tab3" && <p>Content for Tab 3</p>} */}
+        </div>
+
+        <div className="mt-6 flex justify-end">
+          <button
+            onClick={handleSubmitPreferences}
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+          >
+            Save
+          </button>
+        </div>
+      </Modal>
+    </AppLayout>
+  );
+};
+
+export default ForYouNewsPage;
---
+++ src/features/newsHome/pages/index.tsx
@@ -0,0 +1,87 @@
+import { useMemo, useState } from "react";
+import {
+  useGetAllGuardianArticles,
+  useGetNewsAPIArticles,
+  useGetNewsOrgArticles,
+} from "../../../shared/services/";
+import NewsCard from "../../../shared/components/newsCard";
+import { Categories } from "../../userPreferences/config/categories";
+import FiltersBar from "../../../shared/components/filtersBar";
+import { ArticleModel } from "../../../shared/types/articleModel";
+import { normalizeArticles } from "../../../shared/helpers/normalizeArticles";
+import AppLayout from "../../../shared/components/appLayout";
+import { useSearchStore } from "../../../shared/store/searchStore";
+import Loader from "../../../shared/components/loader";
+import NoData from "../../../shared/components/noData";
+
+const NewsHomePage = () => {
+  const { searchValue } = useSearchStore();
+  const [activeCategory, setActiveCategory] = useState(Categories[0].key);
+  const [activeSource, setActiveSource] = useState("");
+  const [dateRange, setDateRange] = useState({
+    start: new Date(),
+    end: new Date(),
+  });
+
+  const { data: newsOrgArticles, isLoading: isNewsOrgArticlesLoading } =
+    useGetNewsOrgArticles({
+      params: {
+        category: activeCategory,
+        q: searchValue,
+      },
+    });
+
+  const { data: guardianArticles, isLoading: isGuardianArticlesLoading } =
+    useGetAllGuardianArticles({
+      params: {
+        categories: activeCategory,
+      },
+    });
+
+  const { data: newsApiArticles, isLoading: isNewsApiArticlesLoading } =
+    useGetNewsAPIArticles({});
+
+  const articles = useMemo(() => {
+    const allArticles = normalizeArticles(
+      newsOrgArticles,
+      newsApiArticles,
+      guardianArticles
+    );
+
+    if (activeSource === "news-api-org") {
+      return allArticles.filter((article) => article.dataSource === "news-org");
+    } else if (activeSource === "guardian-api") {
+      return allArticles.filter((article) => article.dataSource === "guardian");
+    } else if (activeSource === "news-api") {
+      return allArticles.filter((article) => article.dataSource === "news-api");
+    } else {
+      return allArticles;
+    }
+  }, [newsOrgArticles, newsApiArticles, guardianArticles, activeSource]);
+
+  const isLoading =
+    isNewsOrgArticlesLoading ||
+    isNewsApiArticlesLoading ||
+    isGuardianArticlesLoading;
+
+  return (
+    <AppLayout showUpdatePreferences={false}>
+      <FiltersBar
+        activeCategory={activeCategory}
+        setActiveCategory={setActiveCategory}
+        setActiveSource={setActiveSource}
+        dateRange={dateRange}
+        onDateRangeChange={setDateRange}
+      />
+      {isLoading && <Loader />}
+      {!isLoading && articles.length === 0 && <NoData />}
+      <div className="grid md:grid-cols-3 gap-16 mt-36">
+        {articles?.map((article: ArticleModel) => {
+          return <NewsCard article={article} />;
+        })}
+      </div>
+    </AppLayout>
+  );
+};
+
+export default NewsHomePage;
