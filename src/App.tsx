9c306a11fa2ed0b944f2f49445b91a98677d6092
khalil.m.mkh <khalil.m.mkh@gmail.com>
2025-02-23 13:33:41 +0300

    feat - configure app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AppRoutes from "./routes";
+
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div>
-      <header />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />
+    </QueryClientProvider>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,39 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { useRoutes, Navigate } from "react-router-dom";
+import { usePreferencesStore } from "../features/userPreferences/store/preferencesStore";
 import NewsHomePage from "../features/newsHome/pages";
+import ForYouNewsPage from "../features/forYouNews/pages";
+import UserPreferencesPage from "../features/userPreferences/pages";
+import AppLayout from "../shared/components/appLayout";
+import NotFound from "../shared/components/notFound.tsx";
 
 const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/home" replace />} />
-      <Route path="/home" element={<NewsHomePage />} />
-      <Route path="*" element={<h1>404 - Not Found</h1>} />
-    </Routes>
-  );
+  const { isPreferencesSet } = usePreferencesStore();
+
+  return useRoutes([
+    {
+      path: "/preferences",
+      element: !isPreferencesSet ? (
+        <UserPreferencesPage />
+      ) : (
+        <Navigate to="/home" replace />
+      ),
+    },
+
+    {
+      path: "/",
+      element: isPreferencesSet ? (
+        <AppLayout />
+      ) : (
+        <Navigate to="/preferences" replace />
+      ),
+      children: [
+        { path: "home", element: <NewsHomePage /> },
+        { path: "for-you", element: <ForYouNewsPage /> },
+        { path: "404", element: <NotFound /> },
+        { path: "*", element: <Navigate to="/404" replace /> },
+      ],
+    },
+  ]);
 };
 
 export default AppRoutes;
